6076a323522c8ca1cfb8cd63f65f3c8750092315
Linet <muthiigathoni@gmail.com>
2023-07-08 13:52:35 +0300

    added pizza-form

--- script.js
+++ script.js
@@ -1,88 +1,107 @@
-// Fetch menu data from the API
-fetch('https://private-c1294-pizzaapp.apiary-mock.com/restaurants/restaurantId/menu?category=Pizza&orderBy=rank')
-  .then(response => response.json())
-  .then(data => {
-    // Process the menu data and populate the options dynamically
-    const pizzaOptions = data.menu.items;
+document.addEventListener("DOMContentLoaded", () => {
+  // Code to be executed after the DOM is loaded
 
-    const sizeSelection = document.getElementById('sizeSelection');
-    const typeSelection = document.getElementById('typeSelection');
+  // Get form element
+  const form = document.getElementById("form1");
 
-    pizzaOptions.forEach(pizza => {
-      const sizeInput = document.createElement('input');
-      sizeInput.type = 'radio';
-      sizeInput.name = 'size';
-      sizeInput.value = pizza.size;
-      sizeInput.id = pizza.size.toLowerCase();
+  // Get output elements
+  const outputSize = document.getElementById("outputSize");
+  const outputType = document.getElementById("outputType");
+  const outputCrusting = document.getElementById("outputCrusting");
+  const price = document.getElementById("price");
 
-      const sizeLabel = document.createElement('label');
-      sizeLabel.textContent = `${pizza.size} [${pizza.price}]`;
-      sizeLabel.appendChild(sizeInput);
+  // Get deliver button
+  const deliverButton = document.getElementById("deliver");
 
-      sizeSelection.appendChild(sizeLabel);
+  // Get deliver-now button
+  const deliverNowButton = document.getElementById("deliver-now");
 
-      const typeInput = document.createElement('input');
-      typeInput.type = 'radio';
-      typeInput.name = 'type';
-      typeInput.value = pizza.type;
-      typeInput.id = pizza.type.toLowerCase();
+  // Fetch menu data from API
+  fetch("https://private-c1294-pizzaapp.apiary-mock.com/restaurants/2/menu")
+    .then(response => response.json())
+    .then(menuData => {
+      console.log("Menu Data:", menuData);
+      
+      // Add event listener to submit button
+      document.getElementById("submit").addEventListener("click", (event) => {
+        event.preventDefault(); // Prevent form submission
 
-      const typeLabel = document.createElement('label');
-      typeLabel.textContent = `${pizza.type} [${pizza.price}]`;
-      typeLabel.appendChild(typeInput);
+        // Get selected size
+        const sizeSelection = document.querySelector('input[name="size"]:checked');
+        const size = sizeSelection.value;
 
-      typeSelection.appendChild(typeLabel);
+        // Get selected type
+        const typeSelection = document.querySelector('input[name="type"]:checked');
+        const type = typeSelection.value;
+
+        // Get selected crustings
+        const crustings = Array.from(document.querySelectorAll('input[name="crust"]:checked')).map(crust => crust.value);
+
+        // Calculate total cost
+        const totalCost = calculateTotalCost(size, type, crustings, menuData);
+
+        // Update output elements
+        outputSize.textContent = size;
+        outputType.textContent = type;
+        outputCrusting.textContent = crustings.join(", ");
+        price.textContent = totalCost;
+
+        // Show deliver button
+        deliverButton.style.display = "block";
+
+        console.log("Form submitted!");
+        console.log("Size: " + size);
+        console.log("Type: " + type);
+        console.log("Crustings: " + crustings);
+        console.log("Total Cost: " + totalCost);
+      });
+    })
+    .catch(error => {
+      console.log("Error fetching menu data:", error);
     });
-  })
-  .catch(error => {
-    console.error('Error:', error);
+
+  // Add event listener to deliver button
+  deliverButton.addEventListener("click", () => {
+    // Code to deliver the pizza
+    alert("Your pizza will be delivered soon!");
+
+    console.log("Pizza delivered!");
   });
 
-// Event listener for form submission
-document.getElementById('form1').addEventListener('submit', function (event) {
-  event.preventDefault();
+  // Add event listener to deliver-now button
+  document.querySelector('form').addEventListener("submit", (e) => {
+    // Get location details
+    e.preventDefault()
+    let estate = e.target.address.value
+    let phoneNumber = e.target.phoneNumber.value
+    e.target.reset()
 
-  // Get selected values from the form
-  const size = document.querySelector('input[name="size"]:checked');
-  const type = document.querySelector('input[name="type"]:checked');
-  const crusts = Array.from(document.querySelectorAll('input[name="crust"]:checked'));
 
-  // Calculate total cost
-  let totalCost = 0;
-  if (size && type) {
-    totalCost += parseInt(size.value);
-    totalCost += parseInt(type.value);
-  }
-  crusts.forEach(crust => {
-    totalCost += parseInt(crust.value);
+    // Code to deliver the pizza to the specified location
+    alert("Your pizza will be delivered to " + estate + ". Contact number: " + phoneNumber);
+
+    console.log("Pizza delivered to: " + estate);
+    console.log("Contact number: " + phoneNumber);
   });
 
-  // Update output in the order table
-  document.getElementById('outputSize').textContent = size ? size.value : '';
-  document.getElementById('outputType').textContent = type ? type.value : '';
-  document.getElementById('outputCrusting').textContent = crusts.length > 0 ? crusts.map(crust => crust.value).join(', ') : '';
-  document.getElementById('price').textContent = totalCost;
+  // Function to calculate the total cost
+  const calculateTotalCost = (size, type, crustings, menuData) => {
+    const sizeCost = getSizeCost(size, menuData.sizes);
+    const typeCost = getTypeCost(type, menuData.types);
+    const crustingsCost = crustings.length * menuData.crustingsCost;
 
-  // Show the order table
-  document.querySelector('.Order').style.display = 'block';
-});
+    return sizeCost + typeCost + crustingsCost;
+  };
 
-// Event listener for delivery button
-document.getElementById('deliver-now').addEventListener('click', function (event) {
-  event.preventDefault();
+  // Function to get the cost based on the size
+  const getSizeCost = (size, sizes) => {
+    const selectedSize = sizes.find(s => s.name === size);
+    return selectedSize ? selectedSize.price : 0;
+  };
 
-  // Get location details
-  const estate = document.querySelector('input[name="adress"]').value;
-  const phoneNumber = document.querySelector('input[name="phoneNumber"]').value;
-
-  // Perform validation or further processing if required
-
-  // Simulate delivery by displaying a success message
-  const deliveryMessage = document.createElement('p');
-  deliveryMessage.textContent = 'Your order will be delivered to ' + estate + '. Thank you for choosing PizzaPal!';
-  deliveryMessage.style.color = 'green';
-  document.querySelector('.location').appendChild(deliveryMessage);
-
-  // Hide the delivery button
-  document.getElementById('deliver-now').style.display = 'none';
+// Function to get the cost based on the type
+  const getTypeCost = (type, types) => {
+    const selectedType = types.find(t => t.name === type);
+    return selectedType ? selectedType.price : 0;
+  };
 });
